7c4b30420ad290df371c032e03c0fdb6e10b7a02
Fayro1999 <anosikesomadina8@gmail.com>
2024-07-07 23:44:46 -0700

    update settings

--- authent/admin.py
+++ authent/admin.py
@@ -1,10 +1,13 @@
+# admin.py
 from django.contrib import admin
-#from .models import CustomUser
-# Register your models here.
+from .models import CustomUser
+
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name', 'phone', 'is_staff')
+    search_fields = ('username', 'email', 'first_name', 'last_name', 'phone')
 
 
-
-#admin.site.register(CustomUser)
 
 
 
