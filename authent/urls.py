02fcdb0708e17eef4e9225a3b4726f611b3e69cd
Fayro1999 <anosikesomadina8@gmail.com>
2024-07-11 06:48:48 -0700

    update settings

--- authent/urls.py
+++ authent/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('api/authent/register/', RegisterView.as_view(), name='register'),
     path('api/authent/set-password/', SetPasswordView.as_view(), name='set-password'),
     path('api/authent/verify-email/', VerifyEmailView.as_view(), name='verify-email'),
-     path('api/authent/users/', UserListView.as_view(), name='verify-email'),
-      path('api/authent/reference/', ReferenceView.as_view(), name='verify-email'),
+     path('api/authent/users/', UserListView.as_view(), name='users'),
+      path('api/authent/reference/', ReferenceView.as_view(), name='reference'),
     path('api/authent/login/', LoginView.as_view(), name='login'),
 ]
