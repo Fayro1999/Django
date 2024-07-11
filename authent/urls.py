3673e96150d69ead43b1acb4717dac58986c58b3
Fayro1999 <anosikesomadina8@gmail.com>
2024-07-11 11:15:29 -0700

    update settings

--- authent/urls.py
+++ authent/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from .views import RegisterView, SetPasswordView, VerifyEmailView, LoginView, UserListView,  ReferenceView
 
 urlpatterns = [
-    path('api/authent/register/', RegisterView.as_view(), name='register'),
-    path('api/authent/set-password/', SetPasswordView.as_view(), name='set-password'),
-    path('api/authent/verify-email/', VerifyEmailView.as_view(), name='verify-email'),
-     path('api/authent/users/', UserListView.as_view(), name='users'),
-      path('api/authent/reference/', ReferenceView.as_view(), name='reference'),
-    path('api/authent/login/', LoginView.as_view(), name='login'),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('set-password/', SetPasswordView.as_view(), name='set-password'),
+    path('verify-email/', VerifyEmailView.as_view(), name='verify-email'),
+     path('users/', UserListView.as_view(), name='users'),
+      path('reference/', ReferenceView.as_view(), name='reference'),
+    path('login/', LoginView.as_view(), name='login'),
 ]
