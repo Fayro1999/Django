5feedb2251c81bd815c1fa6eeb9d74c1a9f26bc7
Fayro1999 <anosikesomadina8@gmail.com>
2024-07-18 05:11:51 -0700

    setting upload

--- authent/models.py
+++ authent/models.py
@@ -3,6 +3,8 @@ from django.db import models
 
 class CustomUser(AbstractUser):
      phone=models.CharField(max_length=15)
+     verification_code = models.CharField(max_length=6, null=True, blank=True)
+     verification_code_expiration = models.DateTimeField(null=True, blank=True)
    
     
 
