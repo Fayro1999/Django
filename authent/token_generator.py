5d3b8be370542be5351d7d7976d41659e79c8551
Fayro1999 <anosikesomadina8@gmail.com>
2024-07-19 09:59:11 -0700

    setting upload

--- authent/token_generator.py
+++ authent/token_generator.py
@@ -1,36 +1,24 @@
 import random
 from datetime import datetime, timedelta
+from .models import CustomUser
 
 class TokenGenerator:
     def __init__(self, expiration_minutes=10):
         self.expiration_minutes = expiration_minutes
-        self.tokens = {}  # This should be replaced with a more secure storage (e.g., a database)
 
     def make_token(self, user):
         code = ''.join(random.choices('0123456789', k=6))
         expiration_time = datetime.now() + timedelta(minutes=self.expiration_minutes)
-        self.tokens[user.email] = (code, expiration_time)
+        user.verification_code = code
+        user.verification_code_expiration = expiration_time
+        user.save()
         return code
 
     def validate_token(self, email, code):
-        if email not in self.tokens:
-            return False
-
-        stored_code, expiration_time = self.tokens[email]
-        if datetime.now() > expiration_time:
-            return False
-
-        return stored_code == code
-
-    def get_user_from_token(self, email):
-        if email not in self.tokens:
-            return None
-
-        code, expiration_time = self.tokens[email]
-        if datetime.now() > expiration_time:
-            return None
-
         try:
-            return CustomUser.objects.get(email=email)
+            user = CustomUser.objects.get(email=email)
+            if user.verification_code == code and user.verification_code_expiration > datetime.now():
+                return True
         except CustomUser.DoesNotExist:
-            return None
+            return False
+        return False
--- enlist/settings.py
+++ enlist/settings.py
@@ -225,24 +225,3 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'console': {
-            'class': 'logging.StreamHandler',
-        },
-        'file': {
-            'level': 'DEBUG',
-            'class': 'logging.FileHandler',
-            'filename': 'debug.log',
-        },
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['console', 'file'],
-            'level': 'DEBUG',
-            'propagate': True,
-        },
-    },
-}
