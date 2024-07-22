12b343551364d76aa57ea1c49febb4f163bc58c2
Fayro1999 <anosikesomadina8@gmail.com>
2024-07-22 03:36:23 -0700

    updated setting

--- authent/token_generator.py
+++ authent/token_generator.py
@@ -1,24 +1,37 @@
 import random
 from datetime import datetime, timedelta
-from .models import CustomUser
+from django.utils import timezone
 
 class TokenGenerator:
     def __init__(self, expiration_minutes=10):
         self.expiration_minutes = expiration_minutes
+        self.tokens = {}  # This should be replaced with a more secure storage (e.g., a database)
 
     def make_token(self, user):
         code = ''.join(random.choices('0123456789', k=6))
-        expiration_time = datetime.now() + timedelta(minutes=self.expiration_minutes)
-        user.verification_code = code
-        user.verification_code_expiration = expiration_time
-        user.save()
+        expiration_time = timezone.now() + timedelta(minutes=self.expiration_minutes)
+        self.tokens[user.email] = (code, expiration_time)
         return code
 
     def validate_token(self, email, code):
-        try:
-            user = CustomUser.objects.get(email=email)
-            if user.verification_code == code and user.verification_code_expiration > datetime.now():
-                return True
-        except CustomUser.DoesNotExist:
+        if email not in self.tokens:
             return False
-        return False
+
+        stored_code, expiration_time = self.tokens[email]
+        if timezone.now() > expiration_time:
+            return False
+
+        return stored_code == code
+
+    def get_user_from_token(self, email):
+        if email not in self.tokens:
+            return None
+
+        code, expiration_time = self.tokens[email]
+        if timezone.now() > expiration_time:
+            return None
+
+        try:
+            return CustomUser.objects.get(email=email)
+        except CustomUser.DoesNotExist:
+            return None
