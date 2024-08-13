dfaab8e17686c8a45c4be16e32d50f618e70c661
Fayro1999 <anosikesomadina8@gmail.com>
2024-08-13 10:30:31 -0700

    updated setting

--- stores/serializers.py
+++ stores/serializers.py
@@ -1,17 +1,26 @@
 from rest_framework import serializers
 from .models import StoreUserProfile
 from authent.serializers import UserSerializer  # Assuming this serializer handles the user fields
+from django.contrib.auth import get_user_model
+
+CustomUser = get_user_model()
 
 class StoreSerializer(serializers.ModelSerializer):
-    user = UserSerializer()  # Serialize the related user object
+    user = UserSerializer()
 
     class Meta:
         model = StoreUserProfile
-        fields = ['user', 'groups', 'user_permissions']  # Include user-related fields through the user serializer
+        fields = ['user', 'groups', 'user_permissions']
 
     def create(self, validated_data):
         user_data = validated_data.pop('user')
-        user = CustomUser.objects.create(**user_data)
+        user = CustomUser.objects.create(
+            email=user_data['email'],
+            username=user_data['email'],  # Set email as the username
+            first_name=user_data['first_name'],
+            last_name=user_data['last_name'],
+            phone=user_data['phone'],
+            is_active=False  # The user will be inactive until email verification
+        )
         store_user_profile = StoreUserProfile.objects.create(user=user, **validated_data)
         return store_user_profile
-
