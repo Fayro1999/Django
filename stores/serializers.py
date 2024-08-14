55ffc0ba3912f07210bb2f7297145948795f7f27
Fayro1999 <anosikesomadina8@gmail.com>
2024-08-14 00:49:33 -0700

    updated setting

--- stores/serializers.py
+++ stores/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from rest_framework import serializers
+from rest_framework import serializers
 from .models import StoreUserProfile
 from authent.models import CustomUser
 from authent.serializers import UserSerializer  # Assuming this serializer handles the user fields
@@ -9,19 +10,22 @@ class StoreSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = StoreUserProfile
-        fields = ['user', 'groups', 'user_permissions']  # Include user-related fields through the user serializer
+        fields = ['user', 'phone', 'groups', 'user_permissions']  # Include the user-related fields through the user serializer
 
     def create(self, validated_data):
         user_data = validated_data.pop('user')
-        user = CustomUser.objects.create(
+        
+        # Use create_user to properly handle password and user creation
+        user = CustomUser.objects.create_user(
             email=user_data['email'],
             username=user_data['email'],  # Set email as username
             first_name=user_data['first_name'],
             last_name=user_data['last_name'],
             phone=user_data['phone'],
+            password=user_data['password']  # Handle password correctly
         )
-        user.set_password(user_data['password'])
-        user.save()
-
+        
+        # Create the store profile with the newly created user
         store_user_profile = StoreUserProfile.objects.create(user=user, **validated_data)
         return store_user_profile
+
