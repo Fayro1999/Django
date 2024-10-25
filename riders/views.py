bb36de9b0e132f29fa43905d13096c4ca883c916
Fayro1999 <anosikesomadina8@gmail.com>
2024-10-25 06:02:45 -0700

    updated setting

--- riders/views.py
+++ riders/views.py
@@ -1,7 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from .serializers import DispatchRiderSerializer, LoginSerializer
+from .serializers import DispatchRiderSerializer #LoginSerializer
 from rest_framework.permissions import AllowAny, IsAdminUser
 from django.contrib.auth import login
 from .models import DispatchRider
@@ -28,13 +28,13 @@ class LoginDispatchRiderView(APIView):
     permission_classes = [AllowAny]
 
     def post(self, request, *args, **kwargs):
-        username = request.data.get('username')
+        username = request.data.get('email')
         password = request.data.get('password')
         
         if not username or not password:
             return Response({"error": "Username and password are required."}, status=status.HTTP_400_BAD_REQUEST)
         
-        user = authenticate(request, username=username, password=password)
+        user = authenticate(request, username=email, password=password)
         
         if user is not None:
             if user.is_active:
