d1c812b716948673bdf668b7e9a042183e96dbdc
Fayro1999 <anosikesomadina8@gmail.com>
2025-03-28 15:51:06 +0100

    updated settings

--- search/views.py
+++ search/views.py
@@ -2,15 +2,16 @@ from django.http import JsonResponse
 from stores.models import StoreDetails
 from products.models import Product
 from django.db.models import Q
-from stores.serializers import StoreDetailsSerializer  # Importing from the stores app
-from products.serializers import ProductSerializer  # Importing from the products app
+from stores.serializers import StoreDetailsSerializer
+from products.serializers import ProductSerializer
 
-
-# API to search stores and products
 def search_all(request):
-    query = request.GET.get('q', '')
+    query = request.GET.get('q', '').strip()
+    
+    if not query:
+        return JsonResponse({'stores': [], 'products': []}, status=200)
 
-    # Search stores by name, address, city, state
+    # Search stores
     store_results = StoreDetails.objects.filter(
         Q(store_name__icontains=query) |
         Q(store_address__icontains=query) |
@@ -18,18 +19,15 @@ def search_all(request):
         Q(state__icontains=query)
     )
 
-    # Search products by name, description, or store name
+    # Search products (Change store__store_name to vendor__store_name)
     product_results = Product.objects.filter(
         Q(name__icontains=query) |
         Q(description__icontains=query) |
-        Q(store__store_name__icontains=query)
-    )
+        Q(vendor__store_name__icontains=query)  # Fix: Change store to vendor
+    ).select_related('vendor')  # Optimize DB queries
 
     # Serialize data
-    store_data = [{'store_name': store.store_name, 'store_address': store.store_address} for store in store_results]
-    product_data = [{'product_name': product.name, 'store_name': product.store.store_name, 'description': product.description} for product in product_results]
+    store_data = StoreDetailsSerializer(store_results, many=True).data
+    product_data = ProductSerializer(product_results, many=True).data
 
-    return JsonResponse({
-        'stores': store_data,
-        'products': product_data,
-    })
+    return JsonResponse({'stores': store_data, 'products': product_data})
