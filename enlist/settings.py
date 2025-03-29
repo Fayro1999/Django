4a7b17b16f4a6afd89973a2c25df771683486d05
Fayro1999 <anosikesomadina8@gmail.com>
2025-03-29 10:25:01 +0100

    updated settings

--- enlist/settings.py
+++ enlist/settings.py
@@ -265,9 +265,14 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5174",
+    "https://eazziplug.com.ng",
+]
 
-CORS_ALLOW_CREDENTIALS = True 
+CORS_ALLOW_CREDENTIALS = True
+
+CSRF_TRUSTED_ORIGINS = ["http://localhost:5174", "https://eazziplug.com.ng"]
 
 # JWT settings
 JWT_AUTH_COOKIE = 'my-app-auth'
